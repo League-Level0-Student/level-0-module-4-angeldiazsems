8c407100629a87e2077858b98f0f59721d2f7d49
angeldiazsems <56977790+angeldiazsems@users.noreply.github.com>
2020-05-15 13:28:04 -0700

    Add files via upload

---
+++ level-0-module-4-angeldiazsems/src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -0,0 +1,33 @@
+package _01_double._1_change_calculator;
+
+import javax.swing.JOptionPane;
+
+/*
+ * I have a pocket full of change. I hate doing math. Make me a program that
+ * will calculate how much money I have without me having to use my brain.
+ */
+
+public class ChangeCalculator {
+
+	public static void main(String[] args) {
+
+		// Ask the user how many nickels they have
+String input = JOptionPane.showInputDialog("How many nickels do you have/");
+		// Convert their answer to an int.   Hint: Integer.parseInt()  
+int juvenal = Integer.parseInt(input);
+		// Ask the user how many dimes they have, and convert their answer
+String input2 = JOptionPane.showInputDialog("How many dimes do you have?");
+int juvenal2 = Integer.parseInt(input2);
+		// Ask the user how many quarters they have, and convert their answer
+String input3 = JOptionPane.showInputDialog("How many quarters do you have?");
+int juvenal3= Integer.parseInt(input3);
+		// Calculate how much money the user has.  Hint: Use a double variable 
+double hello = juvenal*.05+juvenal2*.10+juvenal3*.25;
+		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
+
+JOptionPane.showMessageDialog(null, "you have "+hello);
+
+
+	}
+}
+
