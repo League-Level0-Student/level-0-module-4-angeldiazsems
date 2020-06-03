7f8e2a9c5914e5301bb40ace3f4d9361aa90ae10
root <root@51ada19cc187>
2020-06-03 21:53:56 +0000

    roblox is better than mincraft

---
+++ src/_04_variables_check/checkpoint.java
@@ -0,0 +1,188 @@
+package _04_variables_check;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class checkpoint {
+public static void main(String[] args) {
+	
+	//module 1 under
+/*	
+String input = JOptionPane.showInputDialog("What is your favorite color?");
+
+
+
+JOptionPane.showMessageDialog(null, input+" is also my favorite color!");
+	
+	
+Robot e = new Robot();
+	
+e.penDown();
+e.setSpeed(90);
+
+for (int i = 0; i < 4; i++) {
+e.move(100);	
+e.turn(120);
+}	
+	
+	*/
+	
+	//module 2 under
+	
+	
+	
+	
+	//String favoritehobby = "programming";
+
+// int numberOftoes = 10;
+
+//int degreesInaRigthangle = 90;
+
+//int myAge = 11;
+
+//String nameofacodinglanguage = "Java";
+
+//double pi = 3.14159;
+
+
+
+
+//String whatIwanttobewhenIgrowup = "soccer player";
+
+//int face ookfriends = 0
+ 
+//String mybirthday = "September";
+
+//int sidesinatriangle = 3;
+
+
+
+//String whatyougotforyourbirthday = "toys";
+
+//String thelastmealyouate = "Hamburgur";
+
+//int whattimeyougotobed = 9:00
+
+//int howmanyrobotprogrammsyouhavewritten = 999;
+
+
+
+
+
+
+
+
+
+/*
+for( int i= 1; i < 11; i++) {
+	
+	
+	System.out.println(i);
+}
+
+
+
+
+
+for( int i= 75; i > 49; i--) {
+	
+	
+	System.out.println(i);
+}
+
+
+
+*/
+
+
+
+
+//for(a;b;c;)
+	
+	
+	
+//a)the first part of the for loop will tell you what it starts at
+	
+//b) the second part tells you what you end at
+		
+//c) the last part tells you if you add or subtract
+	
+	
+	
+	
+	
+	
+	
+
+	
+	/*
+	String input = "yes";
+	
+	
+	
+	if(input .equals("yes")) {
+		
+		JOptionPane.showMessageDialog(null, "HAPPY");
+		
+	}
+	
+		
+		JOptionPane.showMessageDialog(null, "wednesday");
+	
+	
+	*/
+	
+	
+	//if you set it to "no" then it would just say wednesday 
+	
+	
+	
+	
+	
+	int age = 11;
+	
+	if(age <=20) {
+		
+		JOptionPane.showMessageDialog(null, "Isn't it nice being young");
+		
+	}
+	
+	 else{
+		JOptionPane.showMessageDialog(null, "Don't you wish you were young");
+		
+		
+	}
+
+
+
+	
+	
+String input =	JOptionPane.showInputDialog("What is your favorite animal");
+
+
+if(input == "dog") {
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+	
+}
+}
